203d123bc8c007729c94353e24840a8f33c29d5c
Saul Solano <saul3d@yahoo.com>
2019-06-24 18:53:07 -0500

    auth setup

--- src/App/App.js
+++ src/App/App.js
@@ -1,27 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
+import Auth from '../components/Auth/Auth';
+import Home from '../components/Home/Home';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <button className='btn btn-danger'>HELP ME</button>
+    <div>
+      <Auth />
+      <Home />
     </div>
   );
 }
