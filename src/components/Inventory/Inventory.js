a9eb730041221968a30c4866036766a619fb12bb
Saul Solano <saul3d@yahoo.com>
2019-06-25 20:09:59 -0500

    update

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -1,8 +1,18 @@
 import React from 'react';
-
+import fishData from '../../helpers/data/fishData';
 import './Inventory.scss';
 
 class Inventory extends React.Component {
+  state ={
+    fishes: [],
+  }
+
+  componentDidMount() {
+    fishData.getFishes()
+      .then(fishes => this.setState({ fishes }))
+      .catch(err => console.error('Could not get fishes', err));
+  }
+
   render() {
     return (
       <h1>Inventory</h1>
