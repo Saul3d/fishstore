3716b7db1f6494be0824cc7287269586d79b578f
Saul Solano <saul3d@yahoo.com>
2019-06-29 13:43:01 -0500

    added delete order functionality

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,23 +1,55 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
 import Inventory from '../Inventory/Inventory';
 import NewOrders from '../NewOrder/NewOrder';
 import Orders from '../Orders/Orders';
+import fishData from '../../helpers/data/fishData';
+import orderData from '../../helpers/data/orderData';
+
 
 import './Home.scss';
 
 class Home extends React.Component {
+  state = {
+    orders: [],
+    fishes: [],
+  }
+
+  getOrders = () => {
+    orderData.getMyOrders(firebase.auth().currentUser.uid)
+      .then(orders => this.setState({ orders }))
+      .catch(err => console.error('cant get orders', err));
+  }
+
+  componentDidMount() {
+    fishData.getFishes()
+      .then(fishes => this.setState({ fishes }))
+      .catch(err => console.error('Could not get fishes', err));
+
+    this.getOrders();
+  }
+
+  deleteOrder = (orderId) => {
+    orderData.deleteOrder(orderId)
+      .then(() => this.getOrders())
+      .catch(err => console.err('did not delete order', err));
+  }
+
   render() {
+    const { fishes, orders } = this.state;
     return (
       <div className="Home">
         <div className="row">
           <div className="col">
-            <Inventory />
+            <Inventory fishes={fishes} />
           </div>
           <div className="col">
             <NewOrders />
           </div>
           <div className="col">
-            <Orders />
+            <Orders orders={orders} deleteOrder={this.deleteOrder}/>
           </div>
         </div>
       </div>
